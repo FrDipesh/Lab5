35e81889dd55ba869cac607c90f69fad99413200
618989 <poudel.618989@swl.k12.oh.us>
2025-09-18 08:56:26 -0400

    Test6

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scan = new Scanner(System.in);
+        double itemPrice = 0;
+        double shippingRate = 0.02;
+        int FREE_SHIPPING;
+        double shippingCost;
+        double totalCost;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Please enter the price of your item: ");
+        if(scan.hasNextDouble()) {
+            itemPrice = scan.nextDouble();
+        }
+        else{
+            System.out.println("Please enter a valid price: ");
+            System.exit(0);
+        }
+
+        if(itemPrice >= 100) {
+            System.out.println("You spent $100! You get FREE SHIPPING!");
+        }
+        else{
+            shippingCost = itemPrice * shippingRate;
+            totalCost = shippingCost + itemPrice;
+            System.out.println("Your shipping cost is: " + );
         }
     }
 }
