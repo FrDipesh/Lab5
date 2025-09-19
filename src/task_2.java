2a32bc6eece5a84e5f0463d38e3571176f649ea9
618989 <poudel.618989@swl.k12.oh.us>
2025-09-19 08:55:46 -0400

    Save1

--- src/task_2.java
+++ src/task_2.java
@@ -3,7 +3,20 @@ import java.util.Scanner;
 public class task_2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int birthMonth = 0;
 
+        System.out.println("Please enter your birth month ranging from 1-12: ");
+        if (scan.hasNextInt()) {
+            birthMonth = scan.nextInt();
+        }
+        else {
+            System.out.println("You entered an invalid birth month. Please enter a month from 1-12: ");
+            System.exit(0);
+        }
+        System.out.println("Your birth month is: " + birthMonth);
+
+        if () {
+        }
 
     }
 }
